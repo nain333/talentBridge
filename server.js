eb77416071229baaf795fe73acb2d41a8d8a9057
nain333 <hnain70@gmai.com>
2026-05-31 16:32:02 +0530

    feat: configure ejs layout system and home view

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+import "./src/configs/env.configs.js"
+
 import app from "./src/app.js";
-import { configDotenv } from "dotenv";
-configDotenv();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(process.env.PORT)
+  console.log(process.env.PORT);
 });
--- src/app.js
+++ src/app.js
@@ -1,9 +1,39 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import expressLayouts from "express-ejs-layouts";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(process.env.SESSION_SECRET);
 
 // middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(expressLayouts);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 // view engine
+app.set("view engine","ejs")
+app.set("views", path.join(__dirname, "views"));
+app.set("layout", "layouts/layout");
 // routes
+app.get("/", (req, res) => {
+  res.render("home/index", {
+    title: "Home",
+  });
+});
 
 export default app;
